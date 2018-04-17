3d5bb5b985b38ba610c67f3b5a453eeaf69e00e0
AlexV <grafi312@gmail.com>
2018-04-18 03:42:30 +0600

    Deleted mock-news and unused of()

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
 
 import { News } from './news';
-import { NEWS } from './mock-news';
 
 @Injectable()
 export class NewsService {
